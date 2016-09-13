aa0580074dcdfdcaa895265a8efa8f65deffc4a8
SergioBYX <sergioby11@gmail.com>
2016-09-12 22:41:33 -0500

    Pruebas

---
+++ CodigoFacilito/Presentacion2/Presentacion2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            decimal x = 0.0m;
+            float f = 0.0f;
+            double d = 0.0D;
+        }
+    }
+}
